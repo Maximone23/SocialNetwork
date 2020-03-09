cacbe03e25ef87ca00beae6c7c29e4cb8bd84dbb
Maxim Zalutskiy <maxim.one23@gmail.com>
2020-03-10 01:15:03 +0300

    Send Message

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,43 +1,95 @@
-import { act } from "react-dom/test-utils";
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
+const SEND_MESSAGE = 'SEND-MESSAGE';
 
 let store = {
-   _state: {
+  _state: {
     profilePage: {
-      posts: [
-        {id: 1, message: 'Hi, how are you?', likesCount: 15},
-        {id: 2, message: 'It`s my first post', likesCount: 25},
-        {id: 3, message: 'Hi, how are you?', likesCount: 15}
+      posts: [{
+          id: 1,
+          message: 'Hi, how are you?',
+          likesCount: 15
+        },
+        {
+          id: 2,
+          message: 'It`s my first post',
+          likesCount: 25
+        },
+        {
+          id: 3,
+          message: 'Hi, how are you?',
+          likesCount: 15
+        }
       ],
       newPostText: "state string"
     },
     dialogsPage: {
-      dialogs: [
-        {id: 1, name: 'Dmitry'},
-        {id: 2, name: 'Andrey'},
-        {id: 3, name: 'Alexandra'},
-        {id: 4, name: 'Victor'},
-        {id: 5, name: 'Anna'}
+      dialogs: [{
+          id: 1,
+          name: 'Dmitry'
+        },
+        {
+          id: 2,
+          name: 'Andrey'
+        },
+        {
+          id: 3,
+          name: 'Alexandra'
+        },
+        {
+          id: 4,
+          name: 'Victor'
+        },
+        {
+          id: 5,
+          name: 'Anna'
+        }
       ],
-      messages: [
-        {id: 1, message: 'Hi'},
-        {id: 2, message: 'How are you?'},
-        {id: 3, message: 'Yeah'}
-      ]
+      messages: [{
+          id: 1,
+          message: 'Hi'
+        },
+        {
+          id: 2,
+          message: 'How are you?'
+        },
+        {
+          id: 3,
+          message: 'Yeah'
+        }
+      ],
+      newMessageBody: ""
+
     },
     sidebar: {
-      friends: [
-        {id: 1, name: 'Dmitry'},
-        {id: 2, name: 'Andrey'},
-        {id: 3, name: 'Alexandra'},
-        {id: 4, name: 'Victor'},
-        {id: 5, name: 'Anna'}
+      friends: [{
+          id: 1,
+          name: 'Dmitry'
+        },
+        {
+          id: 2,
+          name: 'Andrey'
+        },
+        {
+          id: 3,
+          name: 'Alexandra'
+        },
+        {
+          id: 4,
+          name: 'Victor'
+        },
+        {
+          id: 5,
+          name: 'Anna'
+        }
       ]
     }
-},
+  },
   getState() {
     return this._state;
   },
-  _callsubscriber () {
+  _callsubscriber() {
     console.log("State changed");
   },
   addPost() {
@@ -58,7 +110,7 @@ let store = {
     this._callsubscriber = observer;
   },
   dispatch(action) {
-    if (action.type === 'ADD-POST') {
+    if (action.type === ADD_POST) {
       let newPost = {
         id: 4,
         message: this._state.profilePage.newPostText,
@@ -67,11 +119,33 @@ let store = {
       this._state.profilePage.posts.push(newPost);
       this._state.profilePage.newPostText = '';
       this._callsubscriber(this._state);
-    } else if (action.type === 'UPDATE-NEW-POST-TEXT')
-        this._state.profilePage.newPostText = action.newText;
-        this._callsubscriber(this._state);
+    } else if (action.type === UPDATE_NEW_POST_TEXT) {
+      this._state.profilePage.newPostText = action.newText;
+      this._callsubscriber(this._state);
+    } else if (action.type === UPDATE_NEW_MESSAGE_BODY) {
+      this._state.dialogsPage.newMessageBody = action.body;
+      this._callsubscriber(this._state);
+    } else if (action.type === SEND_MESSAGE) {
+      let body = this._state.dialogsPage.newMessageBody;
+      this._state.dialogsPage.newMessageBody = '';
+      this._state.dialogsPage.messages.push({id: 4, message: body});
+      this._callsubscriber(this._state);
+    }
+
   }
 }
+
+
+
+export const addPostActionCreator = () => ({type: ADD_POST})
+export const updateNewPostTextActionCreator = (text) =>
+  ({type: UPDATE_NEW_POST_TEXT, newText: text})
+
+
+export const sendMessageCreator = () => ({type: SEND_MESSAGE})
+export const updateNewMessageBodyCreator = (body) =>
+  ({type: UPDATE_NEW_MESSAGE_BODY, body: body})
+
 
 
 
