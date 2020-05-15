f4d4e6101321f5d23ebc1eb4b06b0da9cdc90dea
Maxim Zalutskiy <maxim.one23@gmail.com>
2020-05-15 23:07:34 +0300

    refactoring profileReducer and dialogsReducer

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -15,11 +15,11 @@ let mapStateToProps = (state) => {
 
 let mapDispatchToProps = (dispatch) => {
     return {
-        updateNewMessageBody: () => {
-            dispatch(sendMessageCreator());
-            },
-        sendMessage: (body) => {
+        updateNewMessageBody: (body) => {
             dispatch(updateNewMessageBodyCreator(body));
+            },
+        sendMessage: () => {
+            dispatch(sendMessageCreator());
         }
     }
 }
