50306766d8674aca7c570628f959e5d3e9754f06
Maxim Zalutskiy <maxim.one23@gmail.com>
2020-05-19 06:21:36 +0300

    Users API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,44 +1,16 @@
 import React from 'react';
 import styles from './users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/user.png'
 
 const Users = (props) => {
 
     if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                photoUrl: 'https://pixelbox.ru/wp-content/uploads/2018/02/hitman_steam_avatars.png',
-                followed: false,
-                fullName: 'Dmitry',
-                status: 'Developer',
-                location: {
-                    city: 'Moscow',
-                    country: 'Russia'
-                }
-            },
-            {
-                id: 2,
-                photoUrl: 'https://pixelbox.ru/wp-content/uploads/2018/02/hitman_steam_avatars.png',
-                followed: false,
-                fullName: 'Ivan',
-                status: 'Hello',
-                location: {
-                    city: 'Minsk',
-                    country: 'Belarus'
-                }
-            },
-            {
-                id: 3,
-                photoUrl: 'https://pixelbox.ru/wp-content/uploads/2018/02/hitman_steam_avatars.png',
-                followed: true,
-                fullName: 'Alex',
-                status: 'Hastler',
-                location: {
-                    city: 'Kiev',
-                    country: 'Ukraine'
-                }
-            }
-        ])
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                props.setUsers(response.data.items);
+            });
+
     }
 
 
@@ -47,7 +19,7 @@ const Users = (props) => {
             props.users.map(u => <div key={u.id} className={styles.bgUsers}>
         <span>
             <div>
-                <img src={u.photoUrl}  />
+                <img src={u.photos.small != null ? u.photos.small : userPhoto}  />
             </div>
             <div>
                 {u.followed
@@ -57,12 +29,12 @@ const Users = (props) => {
         </span>
                 <span>
             <span>
-                <div>{u.fullName}</div>
+                <div>{u.name}</div>
                 <div>{u.status}</div>
             </span>
             <span>
-                <div>{u.location.country}</div>
-                <div>{u.location.city}</div>
+                <div>{'u.location.country'}</div>
+                <div>{'u.location.city'}</div>
             </span>
         </span>
             </div>)
