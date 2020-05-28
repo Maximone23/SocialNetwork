d32a21cd552c11054e373dd9a549f1564bae943f
Maxim Zalutskiy <maxim.one23@gmail.com>
2020-05-28 03:24:38 +0300

    login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import News from './components/News/News';
 import Music from './components/Music/Music';
@@ -9,6 +8,7 @@ import {Route} from 'react-router-dom';
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 
 
@@ -17,10 +17,10 @@ const App = (props) => {
 
     return (
         <div className="app-wrapper">
-            <Header/>
+            <HeaderContainer/>
             <Navbar/>
             <div className="app-wrapper-content">
-                <Route path='/profile'
+                <Route path='/profile/:userId?'
                        render={() => <ProfileContainer/>}/>
                 <Route path='/dialogs'
                        render={() => <DialogsContainer/>}/>
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from "./users.module.css";
 import userPhoto from "../../assets/images/user.png";
 import {NavLink} from "react-router-dom";
+import * as axios from "axios";
 
 const Users = (props) => {
 
@@ -39,6 +40,15 @@ const Users = (props) => {
                         props.unfollow(u.id)
                     }}>Unfollow</button>
                     : <button onClick={() => {
+
+                        axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                            withCredentials: true
+                        })
+                            .then(response => {
+                                this.props.toggleIsFetching(false);
+                                this.props.setUsers(response.data.items);
+                            });
+
                         props.follow(u.id)
                     }}>Follow</button>}
             </div>
